c86b11c8b71913a1bd2e290b95e1fc4762c0d23d
rachelli99 <914923959@qq.com>
2020-02-27 19:08:12 -0800

    cesar cipher input

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GetInput from './Components/Cesar/Input.js'
 
-function App() {
+class App extends React.Component{
+  
+  handleInput = (e) => {
+
+  }
+  submitted =(e) => {
+
+  }
+  render = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GetInput />
     </div>
   );
+  }
 }
 
 export default App;
