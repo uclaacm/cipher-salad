7b28cf8007554e377a2b28d1d02664fbe5753e36
Alyssa Wang <alywang27@gmail.com>
2020-07-22 22:36:54 -0700

    Got rid of duplicate intro

--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Intro from "../Intro/Intro.js"
 import '../main.css';
 
 function Title(props) {
@@ -13,37 +14,6 @@ function Title(props) {
               </p>
             </div>
           </div>
-        </div>
-        <div className="container">
-          <div className="column">
-              <div className="columns is-vcentered container">
-                <div className="large_font content column container">
-                  <br />
-                  What is encryption? 
-                  How does ZHOFRPH WR JUDYLWB IDOOV turn into WELCOME TO GRAVITY FALLS? 
-                  And how is that related to computers??  
-                </div>
-              </div>
-              <div className="columns is-vcentered container">
-                <div className="large_font content column container">
-                  Computers use cryptography all the time. 
-                  Humankind has encrypted messages since as early as 1500 B.C. 
-                  Let’s start from the beginning... 
-                  <br /><br />
-                 </div>
-              </div> 
-          </div>
-        </div>
-        <div className="row_space">
-          <a href="#caesar_cipher">
-            <button className="button is-large is-family-secondary has-text-weight-bold">Caesar Cipher</button>
-          </a>
-          <a href="#atbash_cipher">
-            <button className="button is-large is-family-secondary has-text-weight-bold">Atbash Cipher</button>
-          </a>
-          <a href="#vigenere_cipher">
-            <button className="button is-large is-family-secondary has-text-weight-bold">Vigenere Cipher</button>
-          </a>
         </div>
       </div>
     );
