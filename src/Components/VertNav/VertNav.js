e8462d0ad3a8c0f97e66560eb77fca059b128a99
krashanoff <lkrashanoff2@gmail.com>
2020-08-21 02:30:35 -0700

    Use colored keys instead of dots.

--- src/Components/VertNav/VertNav.js
+++ src/Components/VertNav/VertNav.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Scrollspy from 'react-scrollspy';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircle, faDotCircle } from '@fortawesome/free-solid-svg-icons';
-import './VertNav.css';
+import { faKey } from '@fortawesome/free-solid-svg-icons';
+import './VertNav.scss';
 
 function NavLink(props) {
   return (
-    <AnchorLink href={props.href}>
-      <FontAwesomeIcon size='2x' icon={String(props.className).includes('current-section') ? faDotCircle : faCircle } />
+    <AnchorLink href={props.href} className={props.className}>
+      <FontAwesomeIcon size='2x' icon={faKey} />
     </AnchorLink>
   );
 }
