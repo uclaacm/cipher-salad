24441c618e8dfde106fe8d1378645568379e2bb8
Alyssa Wang <alywang27@gmail.com>
2020-08-28 21:20:06 -0700

    Slightly stabilizes the wonky blinking of letter boxes

--- src/Components/AtbashIntro/AtbashIntro.js
+++ src/Components/AtbashIntro/AtbashIntro.js
@@ -3,7 +3,6 @@ import Anime from 'react-anime';
 import './atbashIntro.css'
 import { clickMe, partOne, partTwo, partThree} from './AtbashIntroSections.js'
 
-
 class AtbashIntro extends React.Component {
     constructor(props) {
         super(props);
@@ -21,6 +20,8 @@ class AtbashIntro extends React.Component {
     }
 
     handleClickAt = (i) => {
+        if (this.state.clicked[i-1] === true) 
+            return
         let newClick = Array.from(this.state.clicked)
         newClick.splice(i-1, 1, true)
         this.setState({
@@ -54,14 +55,15 @@ class AtbashIntro extends React.Component {
         return(animeProps)
     }
 
+    // on mouseover, switch to 
     render() {
         return(
             <section className="container mb-6">
                 <Anime {...this.animateOnce(true)}>
                     {partOne()}
-                </Anime>
+                </Anime> 
                 <div onMouseOver={() => this.handleClickAt(2)} onClick={() => this.showContent(2)}>
-                    {this.state.show[1] ? <Anime {...this.animateOnce(this.state.finished[1])}>{partTwo()}</Anime> : clickMe("CLICK ME", this.state.clicked[1])}
+                        {this.state.show[1] ? <Anime {...this.animateOnce(this.state.finished[1])}>{partTwo()}</Anime> : clickMe("CLICK ME", this.state.clicked[1])}
                 </div>
                 <div onMouseOver={() => this.handleClickAt(3)} onClick={() => this.showContent(3)}>
                     {this.state.show[2] ? <Anime {...this.animateOnce(this.state.finished[2])}>{partThree()}</Anime> : clickMe("LOOK HERE", this.state.clicked[2])}
