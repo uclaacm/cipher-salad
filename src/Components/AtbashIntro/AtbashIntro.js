53a95d15c072903ed89172bd44c3a99d463a129f
Alyssa Wang <alywang27@gmail.com>
2020-09-07 17:58:11 -0700

    Get rid of timers in atbash intro

--- src/Components/AtbashIntro/AtbashIntro.js
+++ src/Components/AtbashIntro/AtbashIntro.js
@@ -6,67 +6,44 @@ import { clickMe, partOne, partTwo, partThree} from './AtbashIntroSections.js'
 class AtbashIntro extends React.Component {
     constructor(props) {
         super(props);
-        this.timer = 0;
-        this.timers = []
+        const firstPrompt = "click me"
+        const secondPrompt = "click me next :o"
         this.state = {
-            clicked: [true, false, false],
-            finished: [true, false, false],
-            show: [true, false, false]
+            clickList: [clickMe(firstPrompt, false), clickMe(firstPrompt, true), partTwo()],
+            clickList2: [clickMe(secondPrompt, false), clickMe(secondPrompt, true), partThree()],
+            showIndex: [0, 0]
         }
     }
 
-    componentDidUnmount() {
-        this.timer = clearTimeout()
-    }
-
-    handleClickAt = (i) => {
-        if (this.state.clicked[i-1] === true) 
-            return
-        let newClick = Array.from(this.state.clicked)
-        newClick.splice(i-1, 1, true)
-        this.setState({
-            clicked: newClick
-        })
-    }
-
-    showContent = (i) => {
-        let newshow = Array.from(this.state.show)
-        newshow.splice(i-1, 1, true)
-        this.setState({
-            show: newshow
-        })
-        this.timers.push(setTimeout(() => {this.finishAnime(i)}, 1000)) 
-    }
-
-    finishAnime = (i) => {
-        let newFinish = Array.from(this.state.finished)
-        newFinish.splice(i-1, 1, true)
-        this.setState({
-            finished: newFinish
-        })
-    }
-
-    animateOnce = (stop) => {
-        //if condition met, stop animating
-        let animeProps = stop ? {opacity: [1, 1]} : {opacity: [0, 1],
+    animate = () => {
+        let animeProps = {
+            opacity: [1, 1],
             translateX: [-64, 0],
             delay: (el, i) => i * 500
         }
         return(animeProps)
     }
 
-    // on mouseover, switch to 
+    handleShow = (index) => {
+        let currIndex = this.state.showIndex[index]
+        let newIndices = this.state.showIndex
+        newIndices.splice(index, 1, currIndex+1) 
+        if(currIndex < 2) {
+            this.setState({showIndex: newIndices })
+        }
+    }
+
     render() {
+        let { clickList } = this.state
+        let { clickList2 } = this.state
         return(
             <section className="container mb-6">
-                <Anime {...this.animateOnce(true)}>
-                    {partOne()}
-                </Anime> 
-                <div onMouseOver={() => this.handleClickAt(2)} onClick={() => this.showContent(2)}>
-                        {this.state.show[1] ? <Anime {...this.animateOnce(this.state.finished[1])}>{partTwo()}</Anime> : clickMe("CLICK ME", this.state.clicked[1])}
+                {partOne()}
+                <div onClick={()=>this.handleShow(0)}>
+                    {clickList.map((v, i) => (i === this.state.showIndex[0]) ? <Anime {...this.animate()} >{v}</Anime>: <div></div>)}
                 </div>
-                <div onMouseOver={() => this.handleClickAt(3)} onClick={() => this.showContent(3)}>
-                    {this.state.show[2] ? <Anime {...this.animateOnce(this.state.finished[2])}>{partThree()}</Anime> : clickMe("LOOK HERE", this.state.clicked[2])}
+                <div onClick={()=>this.handleShow(1)}>
+                    {clickList2.map((v, i) => (i === this.state.showIndex[1]) ? <Anime {...this.animate()} >{v}</Anime>: <div></div>)}
                 </div>
             </section>
         )
